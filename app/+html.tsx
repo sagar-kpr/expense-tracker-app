510db228e08adf0a826e5230958452608ff2f722
manish kapoor <munish.kapoor@venturepact.com>
2026-07-04 16:06:20 +0530

    fixes

--- app/+html.tsx
+++ app/+html.tsx
@@ -18,7 +18,7 @@ export default function Html({ children }: PropsWithChildren) {
         />
         <meta name="theme-color" content="#16A34A" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/adaptive-icon-foreground.png" />
         <link rel="icon" href="/favicon.png" />
         <ScrollViewStyleReset />
       </head>
